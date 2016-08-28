586f7c9fadba4344db8bd1b55eb3b6ee589d6250
Francisco López Destain <francisco.lopez@fdvsolutions.com>
2016-08-28 17:56:30 -0300

    TP interface to implement is added, and self-generated dummy implementation

---
+++ src/main/java/ar/fiuba/tdd/template/LinkedQueue.java
@@ -0,0 +1,28 @@
+package ar.fiuba.tdd.template;
+
+public class LinkedQueue<T> implements Queue<T> {
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void add(T item) {
+
+    }
+
+    @Override
+    public T top() {
+        return null;
+    }
+
+    @Override
+    public void remove() {
+
+    }
+}
